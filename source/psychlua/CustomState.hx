a9d4b5c4b5f8ed67e8a78b7cff79daa0d08df2f3
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2025-05-27 21:26:58 +0700

    the bug on lua never end :sob:

--- source/psychlua/CustomState.hx
+++ source/psychlua/CustomState.hx
@@ -2,10 +2,20 @@ package psychlua;
 
 // Nothing much since MusicBeatState handle everything now
 class CustomState extends MusicBeatState {
+    public var sillyName:String;
+
     public function new(fileName:String) {
         super();
+        FlxG.camera.bgColor = 0xFF000000;
 
         setStateScript(false);
-        GlobalScriptManager.setStateScript(fileName, this);
+        sillyName = fileName;
+        
+        FlxG.cameras.reset();
+    }
+
+    override function create() {
+        GlobalScriptManager.setStateScript(sillyName, this);
+        super.create();
     }
 }
